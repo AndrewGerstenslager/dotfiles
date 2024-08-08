02a2bc7e756bb0427a2fb2b00986006ad3b2627d
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-08-08 19:41:53 -0400

    added in python REPL support

--- nvim/lua/custom/plugins/dev_area.lua
+++ nvim/lua/custom/plugins/dev_area.lua
@@ -1,5 +1,22 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"pappasam/nvim-repl",
+		init = function()
+			vim.g["repl_filetype_commands"] = {
+				javascript = "node",
+				python = "ipython3 --no-autoindent",
+			}
+		end,
+		keys = {
+			{ "<leader>rt", "<cmd>ReplToggle<cr>", desc = "Toggle nvim-repl" },
+			{ "<leader>rc", "<cmd>ReplRunCell<cr>", desc = "nvim-repl run cell" },
+			{ "<leader>rr", "<Plug>ReplSendLine", desc = "Send line to REPL" },
+			{ "<leader>rv", "<Plug>ReplSendVisual", desc = "Send visual selection to REPL", mode = "x" },
+
+			-- Keymaps for navigating cells
+			{ "<leader>rj", "/#%%<CR>", desc = "Jump to next cell" },
+			{ "<leader>rk", "?#%%<CR>", desc = "Jump to previous cell" },
+			{ "<leader>rn", "o#%%<Esc>", desc = "Insert new cell marker" },
+		},
+	},
+}
