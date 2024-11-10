0a547265241ade228785065db50b3b8789de09a7
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-11-10 18:09:17 -0500

    added minintro for nice clean intro screen to neovim

---
+++ nvim/lua/custom/plugins/minintro.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"eoh-bse/minintro.nvim",
+		opts = {
+			color = "#98c379",
+		},
+		config = true,
+		lazy = false,
+	},
+	{
+		"akinsho/bufferline.nvim", -- optional, if using bufferline
+		opts = {
+			options = {
+				always_show_bufferline = false,
+			},
+		},
+	},
+}
